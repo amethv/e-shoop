f204bb8e3f5d700190ff8c56b3080450c065c880
Amelia Bozano <abozano@est.ups.edu.ec>
2026-07-06 16:06:46 -0500

    Agregado de Autenticación

--- app/models/usuario.py
+++ app/models/usuario.py
@@ -1,6 +1,7 @@
 from app import db 
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 class Usuario(db.Model):
     __tablename__ = 'usuarios'
